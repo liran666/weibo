12d37ce54eaf3181f374b7cdb8d93dae5bf747b6
liran <377586819@qq.com>
2019-06-13 07:09:41 +0000

    页面重置

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>@yield('title','Weibo App') - Laravel 新手入门教程</title>
-</head>
-<body>
-  @yield('content')
-</body>
+<html>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
